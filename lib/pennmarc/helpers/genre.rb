e087a904e53baf02368e91203b2fd69a7cd98a23
Mike Kanning <mkanning@upenn.edu>
2023-06-10 06:41:48 -0400

    Add docs to Genre helper

--- lib/pennmarc/helpers/genre.rb
+++ lib/pennmarc/helpers/genre.rb
@@ -1,13 +1,31 @@
 # frozen_string_literal: true
 
 module PennMARC
-  # Do Genre-y stuff
+  # Genre field values come from the {https://www.oclc.org/bibformats/en/6xx/655.html 655}, but for some
+  # contexts we are only interested in a subset of the declared terms in a record. Some configuration/values
+  # in this helper will be shared with the Subject helper.
   class Genre < Helper
     class << self
+      # Genre values for searching
+      #
+      # @todo port get_genre_search_values
+      # @param [MARC::Record] record
+      # @return [Array]
       def search(record); end
 
+      # Genre values for display
+      #
+      # @todo port get_genre_display, use allowlist
+      # @note legacy method returns a link object
+      # @param [MARC::Record] record
+      # @return [Array]
       def show(record); end
 
+      # Genre values for faceting
+      #
+      # @todo port get_genre_values
+      # @param [MARC::Record] record
+      # @return [Array]
       def facet(record); end
     end
   end
