c5ba5b9349510735d3b6adf1158702a7c9d2420e
Amrey Mathurin <mamrey@upenn.edu>
2023-06-23 08:15:17 -0400

    Update Location helper specs - remove unhelpful contexts - implement more useful contexts - use mock location map - use explicit hash values

--- spec/lib/pennmarc/helpers/location_spec.rb
+++ spec/lib/pennmarc/helpers/location_spec.rb
@@ -4,62 +4,66 @@ describe 'PennMARC::Location' do
   include MarcSpecHelpers
 
   let(:helper) { PennMARC::Location }
-  let(:record) { record_from 'test.xml' }
-
-  describe 'library' do
-    it 'calls location method' do
-      allow(helper).to receive(:location)
-      allow(helper).to receive(:library).and_call_original
-
-      helper.library(record, helper::MAPPINGS)
-
-      expect(helper).to have_received(:location)
-    end
-  end
-
-  describe 'specific_location' do
-    it 'calls location method' do
-      allow(helper).to receive(:location)
-      allow(helper).to receive(:specific_location).and_call_original
-
-      helper.specific_location(record, helper::MAPPINGS)
-
-      expect(helper).to have_received(:location)
-    end
-  end
+  let(:mapping) { location_map }
 
   describe 'location' do
-    it 'returns expected value' do
-      expect(helper.location(record:, location_map: helper::MAPPINGS,
-                             display_value: 'library')).to contain_exactly('LIBRA')
-      expect(helper.location(record:, location_map: helper::MAPPINGS,
-                             display_value: 'specific_location')).to contain_exactly('LIBRA')
+    context "with only 'itm' field present" do
+      let(:record) { marc_record(fields: [marc_field(tag: 'itm', subfields: { g: 'stor' })]) }
+
+      it 'returns expected value' do
+        expect(helper.location(record: record, location_map: mapping,
+                               display_value: :library)).to contain_exactly('LIBRA')
+        expect(helper.location(record:, location_map: mapping,
+                               display_value: 'specific_location')).to contain_exactly('LIBRA')
+      end
+    end
+
+    context "with only 'hld' field present" do
+      let(:record) { marc_record(fields: [marc_field(tag: 'hld', subfields: { c: 'stor' })]) }
+
+      it 'returns expected value' do
+        expect(helper.location(record: record, location_map: mapping,
+                               display_value: :library)).to contain_exactly('LIBRA')
+        expect(helper.location(record: record, location_map: mapping,
+                               display_value: 'specific_location')).to contain_exactly('LIBRA')
+      end
+    end
+
+    context "with both 'hld' and 'itm' fields present" do
+      let(:record) do
+        marc_record(fields: [marc_field(tag: 'itm', subfields: { g: 'stor' }),
+                             marc_field(tag: 'hld', subfields: { c: 'dent' })])
+      end
+      it 'returns item location' do
+        expect(helper.location(record: record, location_map: mapping,
+                               display_value: :library)).to contain_exactly('LIBRA')
+      end
     end
 
     context 'with multiple library locations' do
-      let(:record) { marc_record(fields: [marc_field(tag: 'itm', subfields: { g: %w[stor oovanp] })]) }
+      let(:record) { marc_record(fields: [marc_field(tag: 'itm', subfields: { g: %w[dent] })]) }
 
       it 'returns expected value' do
-        expect(helper.location(record:, location_map: helper::MAPPINGS,
-                               display_value: 'library')).to contain_exactly('LIBRA',
-                                                                             'Van Pelt-Dietrich Library Center')
+        expect(helper.location(record: record, location_map: mapping,
+                               display_value: :library)).to contain_exactly('Health Sciences Libraries',
+                                                                            'Levy Dental Medicine Library')
       end
     end
 
     context 'without enriched marc location tag' do
-      let(:record) { marc_record(fields: [marc_field(tag: '852', subfields: { g: %w[stor oovanp] })]) }
+      let(:record) { marc_record(fields: [marc_field(tag: '852', subfields: { g: 'stor' })]) }
 
       it 'returns expected value' do
-        expect(helper.location(record:, location_map: helper::MAPPINGS, display_value: 'library')).to be_empty
+        expect(helper.location(record: record, location_map: mapping, display_value: :library)).to be_empty
       end
     end
 
     context 'with electronic inventory tag' do
-      let(:record) { marc_record(fields: [marc_field(tag: 'itm', subfields: { g: %w[stor] }), marc_field(tag: 'prt')]) }
+      let(:record) { marc_record(fields: [marc_field(tag: 'itm', subfields: { g: 'stor' }), marc_field(tag: 'prt')]) }
 
       it 'returns expected value' do
-        expect(helper.location(record:, location_map: helper::MAPPINGS,
-                               display_value: 'library')).to contain_exactly('LIBRA', 'Online library')
+        expect(helper.location(record: record, location_map: mapping,
+                               display_value: :library)).to contain_exactly('LIBRA', 'Online library')
       end
     end
   end
