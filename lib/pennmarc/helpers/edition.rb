770da822551fb315085bad9fb807d437dd6d0e98
Mike Kanning <mkanning@upenn.edu>
2023-06-27 17:16:00 -0400

    prep Edition helper for completion

--- lib/pennmarc/helpers/edition.rb
+++ lib/pennmarc/helpers/edition.rb
@@ -4,6 +4,8 @@ module PennMARC
   # Do Edition-y stuff
   class Edition < Helper
     class << self
+      # @param [MARC::Record] record
+      # @return [Array<String>]
       def show(record)
         acc = []
         acc += record.fields('250').map do |field|
@@ -17,6 +19,8 @@ module PennMARC
         acc
       end
 
+      # @param [MARC::Record] record
+      # @return [Array<String>]
       def values(record)
         record.fields('250').take(1).map do |field|
           results = field.find_all(&subfield_not_in(%w[6 8])).map(&:value)
@@ -24,7 +28,30 @@ module PennMARC
         end
       end
 
-      def other_edition_values(_record)
+      # @param [MARC::Record] record
+      # @return [Array<String>]
+      def other_show(record)
+        acc = []
+        acc += record.fields('775')
+                     .select { |f| f.any? { |sf| sf.code == 'i' } }
+                     .map do |field|
+          get_other_edition_value(field)
+        end
+        acc += record.fields('880')
+                     .select { |f| ['', ' '].member?(f.indicator2) }
+                     .select { |f| has_subfield6_value(f, /^775/) }
+                     .select { |f| f.any? { |sf| sf.code == 'i' } }
+                     .map do |field|
+          get_other_edition_value(field)
+        end
+        acc
+      end
+
+      private
+
+      # @param [MARC::DataField] field
+      # @return [String (frozen)]
+      def other_edition_value(field)
         subi = remove_paren_value_from_subfield_i(field) || ''
         other_editions = field.map do |sf|
           if %w[s x z].member?(sf.code)
@@ -40,32 +67,8 @@ module PennMARC
             " (#{sf.value}) "
           end
         end.compact.join
-        {
-          value: other_editions,
-          value_prepend: "#{trim_trailing_period(subi)}:",
-          value_append: other_editions_append,
-          link_type: 'author_creator_xfacet2'
-        }
-      end
-
-      # @todo this one is a bit complicated and returns a link hash
-      # @param [MARC::Record] record
-      # @return [Object]
-      def other_edition_show(record)
-        acc = []
-        acc += record.fields('775')
-                     .select { |f| f.any? { |sf| sf.code == 'i' } }
-                     .map do |field|
-          get_other_edition_value(field)
-        end
-        acc += record.fields('880')
-                     .select { |f| ['', ' '].member?(f.indicator2) }
-                     .select { |f| has_subfield6_value(f, /^775/) }
-                     .select { |f| f.any? { |sf| sf.code == 'i' } }
-                     .map do |field|
-          get_other_edition_value(field)
-        end
-        acc
+        prepend = trim_trailing :period, subi
+        "#{prepend}: #{other_editions} #{other_editions_append}"
       end
     end
   end
