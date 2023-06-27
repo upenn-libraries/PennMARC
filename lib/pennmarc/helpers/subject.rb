4df13a461930a5ff813928b97e5ed14523452974
Mike Kanning <mkanning@upenn.edu>
2023-06-27 17:44:13 -0400

    start work on Subject helper

--- lib/pennmarc/helpers/subject.rb
+++ lib/pennmarc/helpers/subject.rb
@@ -6,12 +6,30 @@ module PennMARC
   # support features (xfacet) that we will no longer support.
   class Subject < Helper
     class << self
+      SEARCH_TAGS = %w[541 561 600 610 611 630 650 651 653].freeze
+      
       # All Subjects for searching
       #
-      # @todo see get_subject_search_values, but there might be more to consider
+      # @note ported from get_subject_search_values
+      # @param [Hash] relator_map
       # @param [MARC::Record] record
       # @return [Array]
-      def search(record); end
+      def search(record, relator_map)
+        record.fields.find_all { |f| is_subject_search_field(f) }
+              .map do |field|
+          subj = []
+          field.each do |sf|
+            if sf.code == 'a'
+              subj << " #{sf.value.gsub(/^%?(PRO|CHR)/, '').gsub(/\?$/, '')}" # TODO: what is this regex doing
+            elsif sf.code == '4'
+              subj << "#{sf.value}, #{relator_map[sf.value]}"
+            elsif !%w[a 4 5 6 8].member?(sf.code)
+              subj << " #{sf.value}"
+            end
+          end
+          join_and_squish(subj) if subj.present?
+        end.compact
+      end
 
       # All Subjects for display
       #
@@ -47,6 +65,23 @@ module PennMARC
       # @param [MARC::Record] record
       # @return [Array]
       def local_show(record); end
+      
+      private
+      
+      # @param [MARC::DataField] field
+      # @return [Boolean]
+      def subject_search_field?(field)
+        if !(field.respond_to?(:indicator2) && %w[0 1 2 4 7].member?(field.indicator2))
+          false
+        elsif SEARCH_TAGS.member?(field.tag) || field.tag.start_with?('69')
+          true
+        elsif field.tag == '880'
+          sub6 = (field.find_all { |sf| sf.code == '6' }.map(&:value).first || '')[0..2]
+          SEARCH_TAGS.member?(sub6) || sub6.start_with?('69')
+        else
+          false
+        end
+      end
     end
   end
 end
---
+++ spec/lib/pennmarc/helpers/subject_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+describe 'PennMARC::Subject' do
+  include MarcSpecHelpers
+
+  let(:helper) { PennMARC::Subject }
+  let(:relator_map) do
+    # TODO
+  end
+
+  describe '.search' do
+
+  end
+
+  describe '.show' do
+
+  end
+
+  describe '.facet' do
+
+  end
+
+  describe '.childrens_show' do
+
+  end
+
+  describe '.medical_show' do
+
+  end
+
+  describe '.local_show' do
+
+  end
+end
