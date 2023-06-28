47010a349984e0539ee35bb0e9867c0fe3c4f094
Mike Kanning <mkanning@upenn.edu>
2023-06-28 07:52:24 -0400

    refactor and initial spec for Subject search method

--- lib/pennmarc/helpers/subject.rb
+++ lib/pennmarc/helpers/subject.rb
@@ -6,29 +6,44 @@ module PennMARC
   # support features (xfacet) that we will no longer support.
   class Subject < Helper
     class << self
+      # Tags that serve as sources for Subject search values
+      # @todo why are 541 and 561 included here?
       SEARCH_TAGS = %w[541 561 600 610 611 630 650 651 653].freeze
-      
-      # All Subjects for searching
-      #
+      # Valid indicator 2 values indicating the source thesaurus for subject terms. These are:
+      # - 0: LCSH
+      # - 1: LC Children's
+      # - 2: MeSH
+      # - 4: Source not specified (local?)
+      # - 7: Source specified in ǂ2
+      SEARCH_SOURCE_INDICATORS = %w[0 1 2 4 7].freeze
+
+      # All Subjects for searching. This includes most subfield content from any field contained in {SEARCH_TAGS} or 69X,
+      # including any linked 880 fields. Fields must have an indicator2 value in {SEARCH_SOURCE_INDICATORS}.
+      # @todo this includes subfields that may not be desired like 1 (uri) and 2 (source code) but this might be OK for
+      #       a search (non-display) field?
       # @note ported from get_subject_search_values
       # @param [Hash] relator_map
       # @param [MARC::Record] record
       # @return [Array]
       def search(record, relator_map)
-        record.fields.find_all { |f| is_subject_search_field(f) }
-              .map do |field|
-          subj = []
-          field.each do |sf|
-            if sf.code == 'a'
-              subj << " #{sf.value.gsub(/^%?(PRO|CHR)/, '').gsub(/\?$/, '')}" # TODO: what is this regex doing
-            elsif sf.code == '4'
-              subj << "#{sf.value}, #{relator_map[sf.value]}"
-            elsif !%w[a 4 5 6 8].member?(sf.code)
-              subj << " #{sf.value}"
+        subject_search_fields(record).filter_map do |field|
+          subj_parts = field.filter_map do |sf|
+            next if sf.code.in? %w[5 6 8]
+
+            # TODO: why do we care about punctuation in a search field? relator mapping?
+            case sf.code
+            when 'a'
+              " #{sf.value.gsub(/^%?(PRO|CHR)/, '').gsub(/\?$/, '')}" # TODO: what is this regex doing?
+            when '4'
+              "#{sf.value}, #{relator_map[sf.value]}"
+            else
+              " #{sf.value}"
             end
           end
-          join_and_squish(subj) if subj.present?
-        end.compact
+          next if subj_parts.empty?
+
+          join_and_squish subj_parts
+        end
       end
 
       # All Subjects for display
@@ -65,22 +80,42 @@ module PennMARC
       # @param [MARC::Record] record
       # @return [Array]
       def local_show(record); end
-      
+
       private
-      
+
+      # Return valid Subject Search fields from a record
+      # @param [MARC::Record] record
+      # @return [Array<MARC::DataField>]
+      def subject_search_fields(record)
+        record.fields.find_all { |field| subject_search_field? field }
+      end
+
+      # Determine if a field should be considered for Subject search inclusion. It must be either contained in
+      # SEARCH_TAGS, be an 880 field otherwise linked to a valid Search tag, or be a 69X field (local subject).
+      # THe indicator 2 of any field cannot be in
       # @param [MARC::DataField] field
       # @return [Boolean]
       def subject_search_field?(field)
-        if !(field.respond_to?(:indicator2) && %w[0 1 2 4 7].member?(field.indicator2))
-          false
-        elsif SEARCH_TAGS.member?(field.tag) || field.tag.start_with?('69')
+        return false if field.blank? || SEARCH_SOURCE_INDICATORS.exclude?(field.indicator2)
+
+        if subject_search_tag?(field.tag)
           true
         elsif field.tag == '880'
-          sub6 = (field.find_all { |sf| sf.code == '6' }.map(&:value).first || '')[0..2]
-          SEARCH_TAGS.member?(sub6) || sub6.start_with?('69')
+          sub6 = field.find_all { |sf| sf.code == '6' }.map(&:value).first
+          subject_search_tag? sub6
         else
           false
         end
+      end
+
+      # Is a given tag a subject search field? Yes if it is contained in SEARCH_TAGS or starts with 69.
+      # @param [String, NilClass] tag
+      # @return [Boolean]
+      def subject_search_tag?(tag)
+        return false if tag.blank?
+
+        tag = tag[0..2]
+        tag&.in?(SEARCH_TAGS) || tag&.start_with?('69')
       end
     end
   end
--- spec/lib/pennmarc/helpers/subject_spec.rb
+++ spec/lib/pennmarc/helpers/subject_spec.rb
@@ -5,11 +5,28 @@ describe 'PennMARC::Subject' do
 
   let(:helper) { PennMARC::Subject }
   let(:relator_map) do
-    # TODO
+    {}
   end
 
   describe '.search' do
+    context 'with a mix of included and excluded tags' do
+      let(:record) do
+        marc_record fields: [
+          marc_field(tag: '600', indicator2: '5', subfields: { a: 'Excluded Canadian' }),
+          marc_field(tag: '610', indicator2: '0', subfields: { a: 'University of Pennsylvania', b: 'Libraries' }),
+          marc_field(tag: '691', indicator2: '7', subfields: { a: 'Van Pelt Library', '2': 'local' }),
+          marc_field(tag: '696', indicator2: '4', subfields: { a: 'A Developer' }),
+          marc_field(tag: '880', indicator2: '0', subfields: { a: 'Alt. Name', '6': '610' })
+        ]
+      end
 
+      it 'does expected stuff' do
+        values = helper.search(record, relator_map)
+        expect(values).to contain_exactly 'A Developer', 'Alt. Name', 'University of Pennsylvania Libraries',
+                                          'Van Pelt Library local'
+        expect(values).not_to include 'Excluded Canadian'
+      end
+    end
   end
 
   describe '.show' do
