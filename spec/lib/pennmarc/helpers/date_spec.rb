382f6045f594b5ed4de70229ee84bec04eab513d
Amrey Mathurin <mamrey@upenn.edu>
2023-07-05 12:27:09 -0400

    Update specs to reflect proper DateTime returns - check for year value - remove test for last_updated ArgumentError

--- spec/lib/pennmarc/helpers/date_spec.rb
+++ spec/lib/pennmarc/helpers/date_spec.rb
@@ -10,7 +10,11 @@ describe 'PennMARC::Date' do
     let(:fields) { [marc_control_field(tag: '008', value: '130827s2010 nyu o 000 1 eng d')] }
 
     it 'returns publication date' do
-      expect(helper.publication(record)).to eq(DateTime.iso8601('2010'))
+      expect(helper.publication(record)).to eq(DateTime.new(2010))
+    end
+
+    it 'returns a year value' do
+      expect(helper.publication(record).year).to eq(2010)
     end
 
     context 'with invalid date' do
@@ -29,6 +33,10 @@ describe 'PennMARC::Date' do
       it 'returns expected value' do
         expect(helper.added(record)).to eq(DateTime.strptime('2023-06-28', '%Y-%m-%d'))
       end
+
+      it 'returns a year value' do
+        expect(helper.added(record).year).to eq(2023)
+      end
     end
 
     context "with date formatted '%Y-%m-%d %H:%M:%S'" do
@@ -36,6 +44,10 @@ describe 'PennMARC::Date' do
 
       it 'returns expected value' do
         expect(helper.added(record)).to eq(DateTime.strptime('2023-06-29 11:04:30:10', '%Y-%m-%d %H:%M:%S'))
+      end
+
+      it 'returns a year value' do
+        expect(helper.added(record).year).to eq(2023)
       end
     end
 
@@ -69,7 +81,7 @@ describe 'PennMARC::Date' do
     let(:fields) { [marc_field(tag: '005', subfields: { q: '20230213163851.0' })] }
 
     it 'returns date last updated' do
-      expect(helper.last_updated(record)).to eq(DateTime.iso8601('20230213163851.0'))
+      expect(helper.last_updated(record)).to eq(DateTime.new('20230213163851.0'.to_i))
     end
 
     context 'with invalid date' do
@@ -77,12 +89,6 @@ describe 'PennMARC::Date' do
 
       it 'returns nil' do
         expect(helper.last_updated(record)).to be_nil
-      end
-
-      it 'outputs error message' do
-        expect do
-          helper.last_updated(record)
-        end.to output("Error parsing last updated date: invalid date - invalid date\n").to_stdout
       end
     end
   end
