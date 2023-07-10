c194e97cd2ad684469f1796171c08f98242322b9
Amrey Mathurin <mamrey@upenn.edu>
2023-07-10 17:13:54 -0400

    Implement Production helper

--- lib/pennmarc/helpers/production.rb
+++ lib/pennmarc/helpers/production.rb
@@ -1,128 +1,135 @@
 # frozen_string_literal: true
 
 module PennMARC
-  # Get fields from the {https://www.oclc.org/bibformats/en/2xx/264.html 264 field}
+  # Extracts data related to a resource's production, distribution, manufacture, and publication.
   class Production < Helper
     class << self
+      # Retrieve production values for display from {https://www.oclc.org/bibformats/en/2xx/264.html 264 field}.
       # @param [MARC::Record] record
       # @return [Array<String>]
       def show(record)
         get_264_or_880_fields(record, '0')
       end
 
+      # Retrieve distribution values for display from {https://www.oclc.org/bibformats/en/2xx/264.html 264 field}.
       # @param [MARC::Record] record
       # @return [Array<String>]
       def distribution_show(record)
         get_264_or_880_fields(record, '2')
       end
 
+      # Retrieve manufacture values for display from {https://www.oclc.org/bibformats/en/2xx/264.html 264 field}.
       # @param [MARC::Record] record
       # @return [Array<String>]
       def manufacture_show(record)
         get_264_or_880_fields(record, '3')
       end
 
+      # Retrieve publication values. Return publication values from
+      # {https://www.oclc.org/bibformats/en/2xx/264.html 264 field} only if none found
+      # {https://www.oclc.org/bibformats/en/2xx/260.html 260}-262 fields.
+      # @param [MARC::Record] record
+      # @return [Array<String>]
       def publication_values(record)
-        acc = []
-        record.fields('245').each do |field|
-          field.find_all { |sf| sf.code == 'f' }
-               .map(&:value)
-               .each { |value| acc << value }
-        end
-        added_2xx = false
-        record.fields(%w[260 261 262]).take(1).each do |field|
-          results = field.find_all { |sf| sf.code != '6' }
-                         .map(&:value)
-          acc << join_and_trim_whitespace(results)
-          added_2xx = true
-        end
-        unless added_2xx
-          sf_ab264 = record.fields.select { |field| field.tag == '264' && field.indicator2 == '1' }
-                           .take(1)
-                           .flat_map do |field|
-            field.find_all(&subfield_in(%w[a b])).map(&:value)
-          end
+        values = []
 
-          sf_c264_1 = record.fields.select { |field| field.tag == '264' && field.indicator2 == '1' }
-                            .take(1)
-                            .flat_map do |field|
-            field.find_all(&subfield_in(['c']))
-                 .map(&:value)
-          end
+        inclusive_dates = record.fields('245').first(1).flat_map { |field| subfield_values(field, 'f') }
 
-          sf_c264_4 = record.fields.select { |field| field.tag == '264' && field.indicator2 == '4' }
-                            .take(1)
-                            .flat_map do |field|
-            field.find_all { |sf| sf.code == 'c' }
-                 .map { |sf| (sf_c264_1.present? ? ', ' : '') + sf.value }
-          end
+        values += inclusive_dates
 
-          acc << [sf_ab264, sf_c264_1, sf_c264_4].join(' ')
+        added_2xx = record.fields(%w[260 261 262])
+                          .first(1)
+                          .map do |field|
+          results = join_subfields(field, &subfield_not_in?(['6']))
+          join_and_squish([results])
         end
-        acc.map!(&:strip).select!(&:present?)
-        acc
+
+        if added_2xx.present?
+          values += added_2xx
+        else
+          fields264 = record.fields('264')
+
+          pub_place_name = fields264
+                           .find(-> { [] }) { |field| field.indicator2 == '1' }
+                           .filter_map { |sf| sf.value if sf.code.in?(%w[a b]) }
+
+          pub_date = fields264
+                     .find(-> { [] }) { |field| field.indicator2 == '1' }
+                     .filter_map { |sf| sf.value if sf.code.in?(['c']) }
+
+          copyright_date = fields264
+                           .find(-> { [] }) { |field| field.indicator2 == '4' }
+                           .filter_map { |sf| "#{pub_date.present? ? ', ' : ''}#{sf.value}" if sf.code.in?(['c']) }
+
+          joined264 = Array.wrap((pub_place_name + pub_date + copyright_date).join(' '))
+
+          values += joined264
+        end
+        values.filter_map { |value| value.strip if value.present? }
       end
 
+      # Retrieve publication values for display from
+      # {https://www.oclc.org/bibformats/en/2xx/264.html 245 field},
+      # {https://www.oclc.org/bibformats/en/2xx/260.html 260}-262, and their linked alternates.
       # @param [MARC::Record] record
       # @return [Object]
       def publication_show(record)
-        acc = []
-        record.fields('245').take(1).each do |field|
-          field.find_all { |sf| sf.code == 'f' }
-               .map(&:value)
-               .each { |value| acc << value }
+        values = []
+
+        values += record.fields('245').first(1).flat_map { |field| subfield_values(field, 'f') }
+
+        values += record.fields(%w[260 261 262]).first(1).map do |field|
+          join_subfields(field, &subfield_not_in?(%w[6 8]))
         end
-        record.fields(%w[260 261 262]).take(1).each do |field|
-          acc << join_subfields(field, &subfield_not_6_or_8)
+
+        values += record.fields('880').filter_map { |field| field if subfield_value?(field, '6', /^(260|261|262)/) }
+                        .first(1).map { |field| join_subfields(field, &subfield_not_in?(%w[6 8])) }
+
+        values += record.fields('880').filter_map do |field|
+          next unless subfield_value?(field, '6', /^245/)
+
+          join_subfields(field, &subfield_in?(['f']))
         end
-        record.fields('880')
-              .select { |f| has_subfield6_value(f, /^(260|261|262)/) }
-              .take(1)
-              .each do |field|
-          acc << join_subfields(field, &subfield_not_6_or_8)
-        end
-        record.fields('880')
-              .select { |f| has_subfield6_value(f, /^245/) }
-              .each do |field|
-          acc << join_subfields(field, &subfield_in(['f']))
-        end
-        acc += get_264_or_880_fields(record, '1')
-        acc.select(&:present?)
+
+        values += get_264_or_880_fields(record, '1')
+        values.compact_blank
       end
 
+      # Retrieve place of publication for display from {https://www.oclc.org/bibformats/en/7xx/752.html 752 field} and
+      # its linked alternate.
+      # @note legacy version returns array of hash objects including data for display link
       # @param [MARC::Record] record
       # @return [Object]
       def place_of_publication_show(record)
-        acc = []
-        acc += record.fields('752').map do |field|
-          place = join_subfields(field, &subfield_not_in(%w[6 8 e w]))
-          place_extra = join_subfields(field, &subfield_in(%w[e w]))
-          { value: place, value_append: place_extra, link_type: 'search' }
+        record.fields(%w[752 880]).filter_map do |field|
+          next if field.tag == '880' && subfield_values(field, '6').exclude?('752')
+
+          place = join_subfields(field, &subfield_not_in?(%w[6 8 e w]))
+          place_extra = join_subfields(field, &subfield_in?(%w[e w]))
+          "#{place} #{place_extra}"
         end
-        acc += get_880_subfield_not_6_or_8(record, '752').map do |result|
-          { value: result, link: false }
-        end
-        acc
       end
 
       private
 
+      # base method to retrieve production values from {https://www.oclc.org/bibformats/en/2xx/264.html 264 field} based
+      # on indicator2.
       # distribution and manufacture share the same logic except for indicator2
       # @param [MARC::Record] record
       # @param [String] indicator2
       # @return [Array<String>]
       def get_264_or_880_fields(record, indicator2)
-        acc = record.fields('264').filter_map do |field|
+        values = record.fields('264').filter_map do |field|
           next unless field.indicator2 == indicator2
 
-          join_subfields(field, &subfield_in(%w[a b c]))
+          join_subfields(field, &subfield_in?(%w[a b c]))
         end
-        acc + record.fields('880').filter_map do |field|
+        values + record.fields('880').filter_map do |field|
           next unless field.indicator2 == indicator2
 
-          next unless has_subfield6_value(field, /^264/)
+          next unless subfield_value?(field, '6', /^264/)
 
-          join_subfields(field, &subfield_in(%w[a b c]))
+          join_subfields(field, &subfield_in?(%w[a b c]))
         end
       end
     end
