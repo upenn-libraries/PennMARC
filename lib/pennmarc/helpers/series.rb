509fb89e0333b22af80f9d2ab74032b74c605332
Patrick Perkins <lilblizzard97@gmail.com>
2023-07-18 15:24:12 -0400

    refactor series helper

--- lib/pennmarc/helpers/series.rb
+++ lib/pennmarc/helpers/series.rb
@@ -4,46 +4,29 @@ module PennMARC
   # Do Series-y stuff
   class Series < Helper
     class << self
-      def show(record)
+      # 800 - Series Added Entry-Personal Name - https://www.loc.gov/marc/bibliographic/bd800.html
+      # 810 - Series Added Entry-Corporate Name - https://www.loc.gov/marc/bibliographic/bd810.html
+      # 410 - Series Statement/Added Entry-Corporate Name - https://www.loc.gov/marc/bibliographic/bd410.html
+      # 811 - Series Added Entry-Meeting Name - https://www.loc.gov/marc/bibliographic/bd811.html
+      # 830 - Series Added Entry-Uniform Title - https://www.loc.gov/marc/bibliographic/bd830.html
+      # 400 - Series Statement/Added Entry-Personal Name - https://www.loc.gov/marc/bibliographic/bd400.html
+      # 411 - Series Statement/Added Entry Meeting Name - https://www.loc.gov/marc/bibliographic/bd411.html
+      # 440 - Series Statement/Added Entry-Title - https://www.loc.gov/marc/bibliographic/bd440.html
+      # 490 - Series Statement - https://www.loc.gov/marc/bibliographic/bd490.html
+      SERIES_TAGS = %w[800 810 811 830 400 411 440 490].freeze
+      def show(record, relator_mapping)
         acc = []
 
-        tags_present = series_tags.select { |tag| record[tag].present? }
+        tags_present = SERIES_TAGS.select { |tag| record[tag].present? }
 
         if %w[800 810 811 400 410 411].member?(tags_present.first)
-          record.fields(tags_present.first).each do |field|
-            # added 2017/04/10: filter out 0 (authority record numbers) added by Alma
-            series = join_subfields(field, &subfield_not_in(%w[0 5 6 8 e t w v n]))
-            pairs = field.map do |sf|
-              if %w[e w v n t].member?(sf.code)
-                [' ', sf.value]
-              elsif sf.code == '4'
-                [', ', relator_codes[sf.value]]
-              end
-            end
-            series_append = pairs.flatten.join.strip
-            acc << { value: series, value_append: series_append, link_type: 'author_search' }
-          end
+          acc += process_first_series_tags(record, tags_present.first, relator_mapping)
         elsif %w[830 440 490].member?(tags_present.first)
-          record.fields(tags_present.first).each do |field|
-            # added 2017/04/10: filter out 0 (authority record numbers) added by Alma
-            series = join_subfields(field, &subfield_not_in(%w[0 5 6 8 c e w v n]))
-            series_append = join_subfields(field, &subfield_in(%w[c e w v n]))
-            acc << { value: series, value_append: series_append, link_type: 'title_search' }
-          end
+          acc += process_second_series_tags(record, tags_present.first)
         end
 
-        record.fields(tags_present.drop(1)).each do |field|
-          # added 2017/04/10: filter out 0 (authority record numbers) added by Alma
-          series = join_subfields(field, &subfield_not_in(%w[0 5 6 8]))
-          acc << { value: series, link: false }
-        end
-
-        record.fields('880')
-              .select { |f| has_subfield6_value(f, /^(800|810|811|830|400|410|411|440|490)/) }
-              .each do |field|
-          series = join_subfields(field, &subfield_not_in(%w[5 6 8]))
-          acc << { value: series, link: false }
-        end
+        acc += process_remaining_fields(record, tags_present)
+        acc += process_880_fields(record)
 
         acc
       end
@@ -67,23 +50,23 @@ module PennMARC
         acc += record.fields(%w[400 410 411])
                      .select { |f| f.indicator2 == '0' }
                      .map do |field|
-          join_subfields(field, &subfield_not_in(%w[4 6 8]))
+          join_subfields(field, &subfield_not_in?(%w[4 6 8]))
         end
         acc += record.fields(%w[400 410 411])
                      .select { |f| f.indicator2 == '1' }
                      .map do |field|
-          join_subfields(field, &subfield_not_in(%w[4 6 8 a]))
+          join_subfields(field, &subfield_not_in?(%w[4 6 8 a]))
         end
         acc += record.fields(%w[440])
                      .map do |field|
-          join_subfields(field, &subfield_not_in(%w[0 5 6 8 w]))
+          join_subfields(field, &subfield_not_in?(%w[0 5 6 8 w]))
         end
         acc += record.fields(%w[800 810 811])
                      .map do |field|
-          join_subfields(field, &subfield_not_in(%w[0 4 5 6 7 8 w]))
+          join_subfields(field, &subfield_not_in?(%w[0 4 5 6 7 8 w]))
         end
         acc += record do |field|
-          join_subfields(field, &subfield_not_in(%w[0 5 6 7 8 w]))
+          join_subfields(field, &subfield_not_in?(%w[0 5 6 7 8 w]))
         end
         acc + record.fields(%w[533])
                     .map do |field|
@@ -104,13 +87,63 @@ module PennMARC
 
       private
 
+      def process_first_series_tags(record, first_tag, relator_mapping)
+        acc = []
+        record.fields(first_tag).each do |field|
+          # added 2017/04/10: filter out 0 (authority record numbers) added by Alma
+          series = join_subfields(field, &subfield_not_in?(%w[0 5 6 8 e t w v n]))
+          pairs = field.map do |sf|
+            if %w[e w v n t].member?(sf.code)
+              [' ', sf.value]
+            elsif sf.code == '4'
+              [', ', translate_relator(sf.value, relator_mapping)]
+            end
+          end
+          series_append = pairs.flatten.join.strip
+          acc << { value: series, value_append: series_append, link_type: 'author_search' }
+        end
+        acc
+      end
+
+      def process_second_series_tags(record, first_tag)
+        acc = []
+        record.fields(first_tag).each do |field|
+          # added 2017/04/10: filter out 0 (authority record numbers) added by Alma
+          series = join_subfields(field, &subfield_not_in?(%w[0 5 6 8 c e w v n]))
+          series_append = join_subfields(field, &subfield_in?(%w[c e w v n]))
+          acc << { value: series, value_append: series_append, link_type: 'title_search' }
+        end
+        acc
+      end
+
+      def process_remaining_fields(record, tags_present)
+        acc = []
+        record.fields(tags_present.drop(1)).each do |field|
+          # added 2017/04/10: filter out 0 (authority record numbers) added by Alma
+          series = join_subfields(field, &subfield_not_in?(%w[0 5 6 8]))
+          acc << { value: series, link: false }
+        end
+        acc
+      end
+
+      def process_880_fields(record)
+        acc = []
+        record.fields('880')
+              .select { |f| subfield_value?(f, '6', /^(800|810|811|830|400|410|411|440|490)/) }
+              .each do |field|
+          series = join_subfields(field, &subfield_not_in?(%w[5 6 8]))
+          acc << { value: series, link: false }
+        end
+        acc
+      end
+
       # logic for 'Continues' and 'Continued By' is very similar
       def get_continues(record, tag)
         record.fields
-              .select { |f| f.tag == tag || (f.tag == '880' && has_subfield6_value(f, /^#{tag}/)) }
-              .select { |f| f.any?(&subfield_in(%w[i a s t n d])) }
+              .select { |f| f.tag == tag || (f.tag == '880' && has_subfield_value(f, '6', /^#{tag}/)) }
+              .select { |f| f.any?(&subfield_in?(%w[i a s t n d])) }
               .map do |field|
-          join_subfields(field, &subfield_in(%w[i a s t n d]))
+          join_subfields(field, &subfield_in?(%w[i a s t n d]))
         end
       end
     end
