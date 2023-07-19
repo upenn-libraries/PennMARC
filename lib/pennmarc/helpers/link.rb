1a7d2e25325f06db3f51179b5dbc4d9d6571e6bb
Amrey Mathurin <mamrey@upenn.edu>
2023-07-19 11:25:43 -0400

    Address more rubocop concerns in Link helper

--- lib/pennmarc/helpers/link.rb
+++ lib/pennmarc/helpers/link.rb
@@ -4,7 +4,6 @@ module PennMARC
   # Do Link-y stuff
   class Link < Helper
     class << self
-
       # @todo the legacy code here is a hot mess for a number of reasons, what do we need this field to do?
       # @note port the needed parts from get_offsite_display, don't return HTML
       # @param [MARC::Record] record
