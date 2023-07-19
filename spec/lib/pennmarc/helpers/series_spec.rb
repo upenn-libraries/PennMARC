739efb216df875c68437a853a381dfc91d8affb5
Patrick Perkins <lilblizzard97@gmail.com>
2023-07-19 15:47:29 -0400

    finish base tests

--- spec/lib/pennmarc/helpers/series_spec.rb
+++ spec/lib/pennmarc/helpers/series_spec.rb
@@ -6,39 +6,45 @@ describe 'PennMARC::Series' do
   let(:helper) { PennMARC::Series }
   let(:mapping) { { aut: 'Author' } }
   let(:record) do
-    marc_record fields: [marc_field(tag: '490', subfields: { 'a': 'Teachings of the feathered serpent' }),
-                         marc_field(tag: '880', subfields: { '6': '490', a: 'Le Teachings' }),
-                         marc_field(tag: '800', subfields: { a: 'Patrick Perkins', d: '1997-', t: 'Teachings', v: 'bk. 1' })]
+    marc_record fields: [marc_field(tag: '490', subfields: { 'a': 'Teachings of the feathered pillow' }),
+                         marc_field(tag: '880', subfields: { '6': '490', a: 'Учения пернатой подушки' }),
+                         marc_field(tag: '800', subfields: { a: 'Bean Bagatolvski', d: '1997-', v: 'bk. 1' }),
+                         marc_field(tag: '780', subfields: { a: 'National Comfort Association' }),
+                         marc_field(tag: '785', subfields: { a: 'NCA quarterly comfortology bulletin' })]
   end
 
   describe '.show' do
     it 'returns the series' do
       expect(helper.show(record, mapping)).to contain_exactly({ link_type: 'author_search',
-                                                                value: 'Patrick Perkins 1997-',
-                                                                value_append: 'Teachings bk. 1' },
-                                                              { link: false, value: 'Teachings of the feathered serpent' },
-                                                              { link: false, value: 'Le Teachings' })
+                                                                value: 'Bean Bagatolvski 1997-',
+                                                                value_append: 'bk. 1' },
+                                                              { link: false, value: 'Teachings of the feathered pillow' },
+                                                              { link: false, value: 'Учения пернатой подушки' })
     end
   end
 
   describe '.values' do
     it 'returns the values' do
-      expect(helper.values(record, mapping)).to contain_exactly('Patrick Perkins 1997- Teachings bk. 1.')
+      expect(helper.values(record, mapping)).to contain_exactly('Bean Bagatolvski 1997- bk. 1.')
     end
   end
 
   describe '.search' do
     it 'returns the search values' do
-      expect(helper.search(record)).to eq('test')
+      expect(helper.search(record)).to contain_exactly('Bean Bagatolvski 1997- bk. 1')
     end
   end
 
   describe '.get_continues_display' do
-
+    it 'gets continues for display' do
+      expect(helper.get_continues_display(record)).to contain_exactly('National Comfort Association')
+    end
   end
 
   describe '.get_continued_by_display' do
-
+    it 'gets continued by display' do
+      expect(helper.get_continued_by_display(record)).to contain_exactly('NCA quarterly comfortology bulletin')
+    end
   end
 end
 
