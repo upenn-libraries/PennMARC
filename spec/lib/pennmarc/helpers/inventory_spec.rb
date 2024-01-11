2bb10f6dde29d6cb529f39dfd74dd1d0d95b3269
Mike Kanning <mkanning@upenn.edu>
2024-01-11 16:12:43 -0500

    add shell of helper spec for Inventory so I don't forget to write specs :)

---
+++ spec/lib/pennmarc/helpers/inventory_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+describe 'PennMARC::Inventory' do
+  include MarcSpecHelpers
+
+  let(:helper) { PennMARC::Inventory }
+
+  describe 'physical'
+  describe 'electronic'
+  describe 'electronic_portfolio_count'
+  describe 'physical_holding_count'
+end
