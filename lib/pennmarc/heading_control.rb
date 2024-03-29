a5e145d20f2c9bb40ace40b7f888c3af621b88b7
Mike Kanning <mkanning@upenn.edu>
2024-03-29 12:35:07 -0400

    add nli to list of approved subject/genre ontologies

--- lib/pennmarc/heading_control.rb
+++ lib/pennmarc/heading_control.rb
@@ -6,6 +6,6 @@ module PennMARC
     # These codes are expected to be found in sf2 when the indicator2 value is 7, indicating "source specified". There
     # are some sources whose headings we don't want to display.
     ALLOWED_SOURCE_CODES = %w[aat cct fast ftamc gmgpc gsafd homoit jlabsh lcgft lcsh lcstt lctgm
-                              local/osu mesh ndlsh nlksh rbbin rbgenr rbmscv rbpap rbpri rbprov rbpub rbtyp].freeze
+                              local/osu mesh ndlsh nli nlksh rbbin rbgenr rbmscv rbpap rbpri rbprov rbpub rbtyp].freeze
   end
 end
