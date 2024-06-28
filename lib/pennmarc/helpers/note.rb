1a2ada13c6fd5827d49aa00005be1566ff7da0d4
Mike Kanning <mkanning@upenn.edu>
2024-06-28 11:35:07 -0400

    adapt contents_show for search field usage by adding an include_vernacular param.

--- lib/pennmarc/helpers/note.rb
+++ lib/pennmarc/helpers/note.rb
@@ -12,7 +12,7 @@ module PennMARC
       # {https://www.oclc.org/bibformats/en/5xx/580.html 580}, {https://www.oclc.org/bibformats/en/5xx/586.html 586},
       # {https://www.oclc.org/bibformats/en/5xx/588.html 588}
       # and their linked alternates.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def notes_show(record)
         notes_fields = %w[500 502 504 515 518 525 533 540 550 580 586 588]
@@ -27,7 +27,7 @@ module PennMARC
       # {https://www.oclc.org/bibformats/en/5xx/562.html 562}, {https://www.oclc.org/bibformats/en/5xx/563.html 563},
       # {https://www.oclc.org/bibformats/en/5xx/585.html 585}, {https://www.oclc.org/bibformats/en/5xx/590.html 590}.
       # Includes linked alternates except for 561.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def local_notes_show(record)
         local_notes = record.fields('561').filter_map do |field|
@@ -50,7 +50,7 @@ module PennMARC
       # prefixed subject field {https://www.oclc.org/bibformats/en/6xx/650.html 650} and its linked alternate.
       # Ignores 561 fields with subfield 'a' values that begin with 'Athenaeum copy: ' and 650 fields where subfield 'a'
       # does not have the prefix 'PRO'.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def provenance_show(record)
         provenance_notes = record.fields(%w[561 880]).filter_map do |field|
@@ -68,20 +68,34 @@ module PennMARC
         notes.uniq
       end
 
-      # Retrieve contents notes for display from fields {https://www.oclc.org/bibformats/en/5xx/505.html 505} and
-      # its linked alternate.
-      # @param [MARC::Record] record
+      # Retrieve contents notes for display from fields {https://www.oclc.org/bibformats/en/5xx/505.html 505} and, if
+      # include_vernacular param is true, its linked alternate. Used for display and searching.
+      # @param record [MARC::Record]
+      # @param include_vernacular [Boolean]
       # @return [Array<String>]
-      def contents_show(record)
+      def contents_values(record, include_vernacular: true)
         record.fields(%w[505 880]).filter_map { |field|
-          next if field.tag == '880' && no_subfield_value_matches?(field, '6', /^505/)
+          if field.tag == '880'
+            next unless include_vernacular
+
+            next if field.tag == '880' && no_subfield_value_matches?(field, '6', /^505/)
+          end
 
           join_subfields(field, &subfield_not_in?(%w[6 8])).split('--')
         }.flatten.uniq
       end
 
+      # Get content note values for searching
+      # @param record [MARC::Record]
+      # @return [Array<String>]
+      def contents_search(record)
+        record.fields('505').map do |field|
+          join_subfields(field, &subfield_in?(%w[a g r t u]))
+        end
+      end
+
       # Retrieve access restricted notes for display from field {https://www.oclc.org/bibformats/en/5xx/506.html 506}.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def access_restriction_show(record)
         record.fields('506').filter_map { |field|
@@ -91,7 +105,7 @@ module PennMARC
 
       # Retrieve finding aid notes for display from field {https://www.oclc.org/bibformats/en/5xx/555.html 555} and its
       # linked alternate.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def finding_aid_show(record)
         datafield_and_linked_alternate(record, '555')
@@ -99,7 +113,7 @@ module PennMARC
 
       # Retrieve participant notes for display from field {https://www.oclc.org/bibformats/en/5xx/511.html 511} and its
       # linked alternate.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def participant_show(record)
         datafield_and_linked_alternate(record, '511')
@@ -107,7 +121,7 @@ module PennMARC
 
       # Retrieve credits notes for display from field {https://www.oclc.org/bibformats/en/5xx/508.html 508} and its
       # linked alternate.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def credits_show(record)
         datafield_and_linked_alternate(record, '508')
@@ -115,7 +129,7 @@ module PennMARC
 
       # Retrieve biography notes for display from field {https://www.oclc.org/bibformats/en/5xx/545.html 545} and its
       # linked alternate.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def biography_show(record)
         datafield_and_linked_alternate(record, '545')
@@ -123,14 +137,14 @@ module PennMARC
 
       # Retrieve summary notes for display from field {https://www.oclc.org/bibformats/en/5xx/520.html 520} and its
       # linked alternate.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def summary_show(record)
         datafield_and_linked_alternate(record, '520')
       end
 
       # Retrieve arrangement values for display from field field {https://www.oclc.org/bibformats/en/3xx/351.html 351}.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def arrangement_show(record)
         datafield_and_linked_alternate(record, '351')
@@ -140,7 +154,7 @@ module PennMARC
       # {https://www.oclc.org/bibformats/en/3xx/344.html 344}, {https://www.oclc.org/bibformats/en/3xx/345.html 345},
       # {https://www.oclc.org/bibformats/en/3xx/346.html 346}, {https://www.oclc.org/bibformats/en/3xx/347.html 347},
       # and their linked alternates.
-      # @param [MARC::Record] record
+      # @param record [MARC::Record]
       # @return [Array<String>]
       def system_details_show(record)
         system_details_notes = record.fields(%w[538 880]).filter_map do |field|
@@ -167,7 +181,7 @@ module PennMARC
       end
 
       # Retrieve "With" notes for display from field {https://www.loc.gov/marc/bibliographic/bd501.html 501}
-      # @param [Marc::Record] record
+      # @param record [Marc::Record]
       # @return [Array<String>]
       def bound_with_show(record)
         record.fields('501').filter_map { |field| join_subfields(field, &subfield_in?(['a'])).presence }.uniq
@@ -177,8 +191,8 @@ module PennMARC
 
       # For system details: extract subfield ǂ3 plus other subfields as specified by passed-in block. Pays special
       # attention to punctuation, joining subfield ǂ3 values with a colon-space (': ').
-      # @param [MARC::DataField] field
-      # @param [Proc] &
+      # @param field [MARC::DataField]
+      # @param & [Proc]
       # @return [String]
       def sub3_and_other_subs(field, &)
         sub3 = field.filter_map { |sf| trim_trailing('period', sf.value) if sf.code == '3' }.join(': ')
