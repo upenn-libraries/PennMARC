c07cadb62b1be80d3b9088072f820ff1c727210d
Mike Kanning <mkanning@upenn.edu>
2025-06-05 12:17:37 -0400

    improve YARD syntax for new method

--- lib/pennmarc/helpers/encoding_rank.rb
+++ lib/pennmarc/helpers/encoding_rank.rb
@@ -7,9 +7,9 @@ module PennMARC
       LEADER_POSITION = 17
 
       # Return a value corresponding to the {https://www.oclc.org/bibformats/en/fixedfield/elvl.html encoding level}
-      # from the MARC leader. Lower numbers indicate a higher level of description. See {encoding_level.rb} for hash
-      # that determines the ranking. We still consider some "legacy" OCLC non-numeric codes here, though they are no
-      # longer recommended for use by OCLC.
+      # from the MARC leader. Lower numbers indicate a higher level of description. See {PennMARC::EncodingLevel} for
+      # hash that determines the ranking. We still consider some "legacy" OCLC non-numeric codes here, though they are
+      # no longer recommended for use by OCLC. If an invalid value is found, nil is returned.
       # @param [MARC::Record] record
       # @return [Integer, nil]
       def sort(record:)
