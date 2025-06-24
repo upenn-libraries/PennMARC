1734db00ae0030a3b77cdb9ed854db6218ebbf72
Mike Kanning <mkanning@upenn.edu>
2025-06-24 16:35:00 -0400

    improve method documentation in Creator helper. move many subfield include/exclude specifications to constants and link to them in method docs.

--- lib/pennmarc/helpers/creator.rb
+++ lib/pennmarc/helpers/creator.rb
@@ -1,14 +1,18 @@
 # frozen_string_literal: true
 
 module PennMARC
-  # Do Creator & Author field processing. Main methods pull from 110 and 111 fields. Display methods here no longer
-  # return data structures intended for generating "search" links, but some of the split subfield parsing remains from
-  # ported methods in case we need to replicate that functionality.
+  # Do Creator & Author field processing. Main methods pull from 110 and 111 fields.
   # @todo can there ever be multiple 100 fields?
   #       can ǂe and ǂ4 both be used at the same time? seems to result in duplicate values
   class Creator < Helper
     # Main tags for Author/Creator information
     TAGS = %w[100 110].freeze
+
+    # For creator fields intended for display, these subfields are ignored
+    DISPLAY_EXCLUDED_SUBFIELDS = %w[a 0 1 4 5 6 8 t].freeze
+
+    # For creator fields intended for searching, these subfields are ignored
+    SEARCH_EXCLUDED_SUBFIELDS = %w[a 1 4 5 6 8 t].freeze
 
     # Aux tags for Author/Creator information, for use in search_aux method
     AUX_TAGS = %w[100 110 111 400 410 411 700 710 711 800 810 811].freeze
@@ -17,6 +21,7 @@ module PennMARC
     CORPORATE_SEARCH_TAGS = %w[110 710 810].freeze
 
     CONTRIBUTOR_TAGS = %w[700 710].freeze
+    CONTRIBUTOR_DISPLAY_SUBFIELDS = %w[a b c d j q u 3].freeze
 
     FACET_SOURCE_MAP = {
       100 => 'abcdjq', 110 => 'abcdjq', 111 => 'abcen',
@@ -25,14 +30,12 @@ module PennMARC
     }.freeze
 
     class << self
-      # Author/Creator search field. Includes all subfield values (even ǂ0 URIs) from
+      # Author/Creator search field, from tags
       # {https://www.oclc.org/bibformats/en/1xx/100.html 100 Main Entry--Personal Name} and
-      # {https://www.oclc.org/bibformats/en/1xx/110.html 110 Main Entry--Corporate Name}. Maps any relator codes found
-      # in ǂ4. To better handle name searches, returns names as both "First Last" and "Last, First" if a comma is found
-      # in ǂa. Also indexes any linked values in the 880.
-      # @todo are we including too many details here and gumming up our index? consider UIRs, relator labels, dates...
-      # @todo shouldn't indicator1 tell us the order of the name? do we not trust the indicator?
-      # @note ported from get_author_creator_1_search_values
+      # {https://www.oclc.org/bibformats/en/1xx/110.html 110 Main Entry--Corporate Name}. Includes subfield values
+      # except for those in the {DISPLAY_EXCLUDED_SUBFIELDS} constant. Maps any relator codes found in ǂ4. To better
+      # handle name searches, returns names as both "First Last" and "Last, First" if a comma is found in ǂa. Also
+      # indexes any linked values in the 880.
       # @param record [MARC::Record]
       # @param relator_map [Hash]
       # @return [Array<String>] array of author/creator values for indexing
@@ -46,18 +49,18 @@ module PennMARC
       # {https://www.loc.gov/marc/bibliographic/bd700.html MARC 700},
       # {https://www.loc.gov/marc/bibliographic/bd710.html MARC 710},
       # and {https://www.loc.gov/marc/bibliographic/bd711.html MARC 711}. The 800, 810 and 8111 tags are similar in
-      # theme to the 7xx fields but apply to serial records.
-      # @note ported from get_author_creator_2_search_values
+      # theme to the 7xx fields but apply to serial records. Includes all subfield values except those in the
+      # {SEARCH_EXCLUDED_SUBFIELDS} constant.
       # @param record [MARC::Record]
       # @return [Array<String>] array of extended author/creator values for indexing
       def search_aux(record, relator_map: Mappers.relator)
         name_search_values record: record, tags: AUX_TAGS, relator_map: relator_map
       end
 
-      # Retrieve creator values for display from fields {https://www.loc.gov/marc/bibliographic/bd100.html 100}
-      # and {https://www.loc.gov/marc/bibliographic/bd110.html 110} and their linked alternates. First, joins subfields
-      # other than $0, $1, $4, $6, $8, $e, and w. Then, appends any encoded relators found in $4.
-      # If there are no valid encoded relators, uses the value found in $e.
+      # Retrieve creator values for display from fields {https://www.loc.gov/marc/bibliographic/bd100.html 100},
+      # {https://www.loc.gov/marc/bibliographic/bd110.html 110} and their linked alternates. First, join each subfield
+      # value except for those defined in the {DISPLAY_EXCLUDED_SUBFIELDS} constant. Then, appends any encoded relators
+      # found in $4. If there are no valid encoded relators, uses the value found in $e.
       # @param record [MARC::Record]
       # @return [Array<String>] array of author/creator values for display
       def show(record, relator_map: Mappers.relator)
@@ -88,13 +91,7 @@ module PennMARC
       # @param first_initial_only [Boolean] only use the first initial instead of first name
       # @return [Array<String>] names of the authors
       def authors_list(record, main_tags_only: false, first_initial_only: false)
-        tags = if main_tags_only
-                 TAGS
-               else
-                 TAGS + CONTRIBUTOR_TAGS
-               end
-
-        fields = record.fields(tags)
+        fields = record.fields(main_tags_only ? TAGS : TAGS + CONTRIBUTOR_TAGS)
         fields.filter_map { |field|
           if field['a'].present?
             name = trim_trailing(:comma, field['a'])
@@ -161,7 +158,7 @@ module PennMARC
         }.uniq
       end
 
-      # Author/Creator sort. Does not map and include any relator codes.
+      # Author/Creator sort. Does not map or include any relator codes.
       # @todo This includes any URI from ǂ0 which could help to disambiguate in sorts, but ǂ1 is excluded...
       # @note ported from get_author_creator_sort_values
       # @param record [MARC::Record]
@@ -174,8 +171,6 @@ module PennMARC
       # Author/Creator for faceting. Grabs values from a plethora of fields, joins defined subfields, then trims some
       # punctuation (@see Util.trim_punctuation)
       # @todo should trim_punctuation apply to each subfield value, or the joined values? i think the joined values
-      # @note ported from author_creator_xfacet2_input - is this the best choice? check the copyField declarations -
-      #       franklin uses author_creator_f
       # @param record [MARC::Record]
       # @return [Array<String>] array of author/creator values for faceting
       def facet(record)
@@ -264,7 +259,7 @@ module PennMARC
 
       # Retrieve contributor values for display from fields {https://www.oclc.org/bibformats/en/7xx/700.html 700}
       # and {https://www.oclc.org/bibformats/en/7xx/710.html 710} and their linked alternates. Joins subfields
-      # 'a', 'b', 'c', 'd', 'j', and 'q', 'u', and '3'. Then appends resulting string with any encoded relationships
+      # defined in {CONTRIBUTOR_DISPLAY_SUBFIELDS}, then appends resulting string with any encoded relationships
       # found in $4. If there are no valid encoded relationships, uses the value found in $e.
       # @note legacy version returns array of hash objects including data for display link
       # @param record [MARC::Record]
@@ -278,7 +273,7 @@ module PennMARC
         if vernacular
           fields += record.fields('880').select { |f| subfield_value?(f, '6', /^(#{CONTRIBUTOR_TAGS.join('|')})/) }
         end
-        sf = name_only ? %w[a] : %w[a b c d j q u 3]
+        sf = name_only ? %w[a] : CONTRIBUTOR_DISPLAY_SUBFIELDS
         fields.filter_map { |field|
           next if indicator_2_options.exclude?(field.indicator2) && field.tag.in?(CONTRIBUTOR_TAGS)
           next if subfield_defined? field, 'i'
@@ -323,7 +318,7 @@ module PennMARC
       # @param for_display [Boolean]
       # @return [String] joined subfield values for value from field
       def name_from_main_entry(field, mapping, should_convert_name_order: false, for_display: true)
-        subfield_exclude_spec = for_display ? %w[a 0 1 4 5 6 8 t] : %w[a 1 4 5 6 8 t]
+        subfield_exclude_spec = for_display ? DISPLAY_EXCLUDED_SUBFIELDS : SEARCH_EXCLUDED_SUBFIELDS
         relator_term_sf = relator_term_subfield(field)
         name = field.filter_map { |sf|
           if sf.code == 'a'
